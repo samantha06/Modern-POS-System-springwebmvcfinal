fabaf457be34703e0ce3000c3be2d7472954e1d4
Samantha <psd0622@gmail.com>
2019-06-21 07:18:53 +0530

    Finish POS System with spring web MVC

---
+++ 1-springwebmvcfinal/src/main/java/lk/ijse/dep/webmvc/main/WebAppConfig.java
@@ -0,0 +1,12 @@
+package lk.ijse.dep.webmvc.main;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@ComponentScan("lk.ijse.dep.webmvc")
+@Configuration
+public class WebAppConfig {
+}
---
+++ 1-springwebmvcfinal/src/main/java/lk/ijse/dep/webmvc/main/WebRootConfig.java
@@ -0,0 +1,9 @@
+package lk.ijse.dep.webmvc.main;
+
+
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class WebRootConfig {
+}
